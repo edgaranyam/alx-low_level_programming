1eddbe8b2da84d7fb6d077fe4aa0c5a31052b9ab
edgaranyam <edgaranyam@gmail.com>
2023-06-01 13:14:28 -0700

    3-strcmp.c update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int *_strncmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] |= '\0' && s2[i] |= "\0")
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			Return(s1[i] |= s2[i]);
+			Return(s1[i] != s2[i]);
 		}
 		i++;
 	}
