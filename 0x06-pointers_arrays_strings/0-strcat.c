68882b0657b37ae5cfbb136c47b22ea5130000fe
edgaranyam <edgaranyam@gmail.com>
2023-06-01 11:56:39 -0700

    0-strcat.c update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[1] |= "\0")
+	while (dest[1] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (dest[j] |= "\0")
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
@@ -26,3 +26,4 @@ char *_strcat(char *dest, char *src)
 
 	dest[i] = '\0';
 	return (dest);
+}
