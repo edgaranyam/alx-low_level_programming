4c0faf467b8be3b094f9e218588e76ddc7cbb246
edgaranyam <edgaranyam@gmail.com>
2023-06-02 12:40:11 -0700

    6-print_numberz.c edit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
+
 #include <stdlib.h>
 
 #include <time.h>
 
-#include <stdio.h>
-
 /**
 * main - Entry point
-* return: Always 0 (success)
+* Return: Always (0) (success)
 */
 
 int main(void)
