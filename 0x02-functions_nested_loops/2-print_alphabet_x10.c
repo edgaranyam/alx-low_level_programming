024e095a18c7089746c183c116c0ed606148eb55
edgaranyam <edgaranyam@gmail.com>
2023-06-03 12:08:41 -0700

    2-print_alphabet_x10.c correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10_times - make alphabet x10
+ * print_alphabet_x10 - make alphabet x10
  *
  * Return: void
  */
@@ -9,16 +9,17 @@
 void print_alphabet_x10(void)
 {
 
-	char ch;
-	int i;
-	i = 0;
-	while (i < 10)
-	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	_putchar(ch);
-	}
-	_putchar('\n');
-	}
-	i++;
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+i++;
+
 }
